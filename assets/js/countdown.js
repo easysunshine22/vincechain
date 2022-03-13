ce563d358198999d65a6de0e3b42db95737e1bcb
webdev0202 <webdev0202@yandex.com>
2022-03-13 20:00:56 +0100

    new commit

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -47,5 +47,5 @@ function getTimeRemaining(endtime) {
     var timeinterval = setInterval(updateClock, 1000);
   }
 
-  var deadline = "March 13 2022 08:00:00 GMT+0000";
+  var deadline = "March 15 2022 08:00:00 GMT+0000";
   initializeClock("countdown", deadline);
